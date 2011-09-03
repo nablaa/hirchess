6a12becf4cef9405eecbf11c6662eaf9741fdcbb
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2011-09-03 22:21:33 +0300

    Add Piece.hs stub

---
+++ Piece.hs
@@ -0,0 +1,22 @@
+module Piece (Piece, Color, printPiece) where
+
+import Data.Char
+
+data Piece = Piece Type Color
+             deriving (Eq, Show, Read)
+
+data Color = White | Black
+             deriving (Eq, Show, Read)
+
+data Type = Pawn | Knight | Bishop | Rook | Queen | King
+            deriving (Eq, Show, Read)
+
+pieceChars :: [(Type, Char)]
+pieceChars = [(Pawn, 'P'), (Knight, 'N'), (Bishop, 'B'), (Rook, 'R'), (Queen, 'Q'), (King, 'K')]
+
+printPiece :: Piece -> Char
+printPiece (Piece t color) = case color of
+                               White -> toUpper c
+                               Black -> toLower c
+    where (Just c) = lookup t pieceChars
+
