2e58f8e56d102d262c1a7e69952a0a01db21afc7
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2011-09-14 23:07:28 +0300

    Add stub for Game.hs

---
+++ Game.hs
@@ -0,0 +1,24 @@
+module Game (GameState, initialState) where
+
+import Board
+import Piece
+import Move
+
+initialState :: GameState
+initialState = (State initialBoard White initialCastlings Nothing 0 1)
+    where initialCastlings = [Long White, Short White, Long Black, Short Black]
+
+hasEnded :: GameState -> Bool
+hasEnded = undefined
+
+getWinner :: GameState -> Maybe Color
+getWinner = undefined
+
+isDraw :: GameState -> Bool
+isDraw = undefined
+
+applyMove :: GameState -> Move -> GameState
+applyMove = undefined
+
+debugPrintState :: GameState -> IO ()
+debugPrintState = undefined
