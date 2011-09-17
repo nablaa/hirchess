9fc0c7c63a771bdeb9bf2db5aa2386c269ab7a08
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2011-09-17 17:30:57 +0300

    Add parsing of FEN notation

--- FEN.hs
+++ FEN.hs
@@ -1,5 +1,7 @@
 module FEN (writeFEN, readFEN) where
 
+import Data.Char
+import Data.Maybe
 import Data.List
 import Move
 import Board
@@ -29,4 +31,47 @@ writeFEN :: GameState -> String
 writeFEN state = unwords [writeBoard (board state), writePlayer (player state), writeCastlings (castlingsPossible state), writeEnPassant (enPassantSquare state), show (halfmoveClock state), show (moveNumber state)]
 
 readFEN :: String -> Maybe GameState
-readFEN = undefined
+readFEN str | length parts /= 6 = Nothing
+            | otherwise = do board <- readBoard $ parts !! 0
+                             player <- readPlayer $ parts !! 1
+                             castlings <- readCastlings $ parts !! 2
+                             enPassant <- readEnPassant $ parts !! 3
+                             halfmoves <- maybeRead $ parts !! 4
+                             moves <- maybeRead $ parts !! 5
+                             return $ State board player castlings enPassant halfmoves moves
+    where parts = words str
+
+readBoard :: String -> Maybe Board
+readBoard str | length parts /= 8 = Nothing
+              | otherwise = parseBoard $ unlines parts
+    where numToEmpty x | isNumber x = replicate (digitToInt x) ' '
+                       | otherwise = [x]
+          parts = split (== '/') $ concat $ map numToEmpty str
+
+readPlayer :: String -> Maybe Color
+readPlayer "w" = Just White
+readPlayer "b" = Just Black
+readPlayer _ = Nothing
+
+readCastlings :: String -> Maybe [Castling]
+readCastlings = sequence . map toCastling
+    where toCastling 'Q' = Just $ Long White
+          toCastling 'K' = Just $ Short White
+          toCastling 'q' = Just $ Long Black
+          toCastling 'k' = Just $ Short Black
+          toCastling _ = Nothing
+
+readEnPassant :: String -> Maybe (Maybe Coordinates)
+readEnPassant "-" = Just Nothing
+readEnPassant str = case stringToCoordinates str of
+                      Nothing -> Nothing
+                      coordinates -> Just (fmap toEnPassantTargetSquare coordinates)
+
+maybeRead :: Read a => String -> Maybe a
+maybeRead = fmap fst . listToMaybe . filter (null . snd) . reads
+
+split :: (Char -> Bool) -> String -> [String]
+split p str = case dropWhile p str of
+                "" -> []
+                str' -> w : split p str''
+                    where (w, str'') = break p str'
