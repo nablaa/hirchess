517e2088957345d272501332a0fbde2340f2bd1e
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2011-09-24 19:15:12 +0300

    Add printing of algebraic notation

--- Notation.hs
+++ Notation.hs
@@ -1,5 +1,6 @@
 module Notation (NotationParser, NotationPrinter,
                  printLongAlgebraicNotation,
+                 printAlgebraicNotation,
                  printCoordinateNotation, parseCoordinateNotation,
                  printMoveList, printMoveListColumns) where
 
@@ -30,6 +31,17 @@ printLongAlgebraicNotation (Move (Castling (Short _)) _ _ _) = "O-O"
 printLongAlgebraicNotation move@(Move (EnPassant _) _ _ _) = longAlgebraicNotation' move "x"
 printLongAlgebraicNotation move@(Move (Promotion (Piece promoted _)) _ _ _) = longAlgebraicNotation' move "-" ++ printPiece (Piece promoted White)
 printLongAlgebraicNotation move@(Move PawnDoubleMove _ _ _) = longAlgebraicNotation' move "-"
+
+printAlgebraicNotation :: NotationPrinter
+printAlgebraicNotation (Move Movement (Piece Pawn _) _ end) = coordinatesToString end
+printAlgebraicNotation (Move PawnDoubleMove (Piece Pawn _) _ end) = coordinatesToString end
+printAlgebraicNotation (Move Capture (Piece Pawn _) start end) = head (coordinatesToString start) : "x" ++ coordinatesToString end
+printAlgebraicNotation (Move (EnPassant _) _ start end) = head (coordinatesToString start) : "x" ++ coordinatesToString end ++ "e.p"
+printAlgebraicNotation (Move Movement piece _ end) = map toUpper (printPiece piece) ++ coordinatesToString end
+printAlgebraicNotation (Move Capture piece _ end) = map toUpper (printPiece piece) ++ "x" ++ coordinatesToString end
+printAlgebraicNotation (Move (Castling (Long _)) _ _ _) = "O-O-O"
+printAlgebraicNotation (Move (Castling (Short _)) _ _ _) = "O-O"
+printAlgebraicNotation (Move (Promotion (Piece promoted _)) _ _ end) = coordinatesToString end ++ printPiece (Piece promoted White)
 
 printCoordinateNotation :: NotationPrinter
 printCoordinateNotation (Move _ _ coord1 coord2) = map toUpper $ coordinatesToString coord1 ++ "-" ++ coordinatesToString coord2
