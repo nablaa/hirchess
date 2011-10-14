365f80cd4b42b4daf4335dc3cd2b0aa9fe8c4ca5
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2011-10-14 23:47:04 +0300

    Add basic functions to use IRC

---
+++ IRC.hs
@@ -0,0 +1,45 @@
+module IRC (initialize, readChannel, writeChannel) where
+
+import Network
+import System.IO
+import Text.Printf
+import Data.List
+import System.Exit
+
+server = "irc.freenode.org"
+port = 6667
+chan = "#hirchess"
+nick = "hirchess"
+user = "hirchess"
+
+
+initialize :: IO Handle
+initialize = do h <- connectTo server (PortNumber (fromIntegral port))
+                hSetBuffering h NoBuffering
+                write h "NICK" nick
+                write h "USER" (nick ++ " 0 * :" ++ user)
+                write h "JOIN" chan
+                return h
+
+write :: Handle -> String -> String -> IO ()
+write h s t = do
+  hPrintf h "%s %s\r\n" s t
+  printf    "> %s %s\n" s t
+
+readChannel :: Handle -> IO String
+readChannel h = do t <- hGetLine h
+                   let s = init t
+                   putStrLn s
+                   if ping s then pong s >> readChannel h else
+                       if correctChannel s then
+                           return (clean s)
+                       else
+                           readChannel h
+    where clean = drop 1 . dropWhile (/= ':') . drop 1
+          ping x = "PING :" `isPrefixOf` x
+          pong x = write h "PONG" (':' : drop 6 x)
+          correctChannel str = length (words str) >= 4 && (words str) !! 2 == chan
+
+writeChannel :: Handle -> String -> IO ()
+writeChannel _ "" = return ()
+writeChannel h s = write h "PRIVMSG" (chan ++ " :" ++ s)
